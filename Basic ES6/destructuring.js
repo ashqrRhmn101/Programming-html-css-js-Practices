717ee3622d7108b6fde28529a5bf78707017e1cb
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-31 13:57:16 +0600

    ES6 Basic

--- Basic ES6/destructuring.js
+++ Basic ES6/destructuring.js
@@ -1,4 +1,17 @@
-const product = {name: "shirt", price: 500, quantity: 20};
+const product = [
+    {name: "shirt", price: 500, quantity: 2},
+    {name: "Pant", price: 700, quantity: 10},
+    {name: "Tupi", price: 800, quantity: 4},
+    {name: "Belt", price: 900, quantity: 8},
+
+];
+let totalPrices = 0;
+
+for(let {name, price} of product){
+    console.log(`${name} Price ${price}`);
+    totalPrices += price;
+}
+console.log("Total Price Product: " , totalPrices); 
 
 // // const discount = product.price*0.8; //20%
 // const discount = product.price*20/100;
@@ -9,10 +22,10 @@ const product = {name: "shirt", price: 500, quantity: 20};
 // // const price = product.price; (shortCut)
 
 // console.log(totalPrice);
+  
+// NEXTTT      
 
-// NEXTTT 
-
-const {price , quantity} = {name: "shirt", price: 500, quantity: 5};
+const {price , quantity} = {name: "shirt", price: 500, quantity: 3};
 
 const discount = price*20/100;
 const yourPay = price-discount;
@@ -20,5 +33,5 @@ const vat = yourPay*7/100;
 const totalPrice = vat+yourPay;
 const quntty = totalPrice*quantity;
 
-console.log(`Total price = ${totalPrice} 
+console.log(`Total Shirt price = ${totalPrice} 
 Quantity ${quantity} Total = ${quntty} `);
--- Basic ES6/seal-freeze-delete.js
+++ Basic ES6/seal-freeze-delete.js
@@ -1,10 +1,27 @@
 const employee = {
     name: "Raja rani",
+    age: 0 ,
     designation: "QA",
-    salary: "20000",
-    experience: 1
+    salary: 20000,
+    experience: 1,
+    location: ""
 }
 
-delete employee.salary;
+// delete employee.salary;
+// delete employee.name;
+
+// Object.seal(employee); // Add 
+
+// employee.salary = employee.salary+ 30000;
+// employee.age = 40;
+// employee.location = 'Dhaka';
+
+// console.log(employee);
+
+Object.freeze(employee); // Don't Add
+
+employee.salary = employee.salary+ 30000;
+employee.age = 40;
+employee.location = 'Dhaka';
 
 console.log(employee);
